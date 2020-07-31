9377972229faaa6ddd17380e2113164bc9d974d4
Dmitry Marov <d.marov94@gmail.com>
2020-07-31 20:33:42 +0300

    update

--- src/modules/eventloop.cpp
+++ src/modules/eventloop.cpp
@@ -1,4 +1,33 @@
 #include "eventloop.h"
+#include <GL/glut.h>
+#include <iostream>
+
+void EventLoop::start(int argc, char **argv) {
+
+    std::string window_name = "FPS";
+
+    glutInit(&argc, argv);
+
+    int screen_width = glutGet(GLUT_SCREEN_WIDTH);
+    int screen_height = glutGet(GLUT_SCREEN_HEIGHT);
+
+    glutInitWindowSize(screen_width, screen_height);
+    glutInitWindowPosition(0,0);
+    glutInitDisplayMode(GLUT_DOUBLE && GLUT_RGBA);
+
+    int window_id = glutCreateWindow(window_name.c_str());
+
+    glutSetWindowTitle(window_name.c_str());
+    glutSetIconTitle(window_name.c_str());
+    glutFullScreen();
+    glutSetCursor(GLUT_CURSOR_NONE);
+    glutDisplayFunc(&EventLoop::on_display);
+    glutKeyboardFunc(&EventLoop::on_keyboard);
+    glutMouseFunc(&EventLoop::on_mouse);
+    glutSpecialFunc(&EventLoop::on_special);
+    glutMainLoop();
+
+}
 
 void EventLoop::on_display() {
 
--- src/modules/eventloop.h
+++ src/modules/eventloop.h
@@ -1,39 +1,12 @@
-#ifndef EVENTLOOP_H_
-#define EVENTLOOP_H_
+#ifndef __EVENTLOOP_H__
+#define __EVENTLOOP_H__
 
-#include <GL/glut.h>
-#include <iostream>
 #include <string>
 
 class EventLoop {
 
 public:
-    static void start(int argc, char **argv) {
-
-        std::string window_name = "FPS";
-
-        glutInit(&argc, argv);
-
-        int screen_width = glutGet(GLUT_SCREEN_WIDTH);
-        int screen_height = glutGet(GLUT_SCREEN_HEIGHT);
-
-        glutInitWindowSize(screen_width, screen_height);
-        glutInitWindowPosition(0,0);
-        glutInitDisplayMode(GLUT_DOUBLE && GLUT_RGBA);
-
-        int window_id = glutCreateWindow(window_name.c_str());
-
-        glutSetWindowTitle(window_name.c_str());
-        glutSetIconTitle(window_name.c_str());
-        glutFullScreen();
-        glutSetCursor(GLUT_CURSOR_NONE);
-        glutDisplayFunc(&EventLoop::on_display);
-        glutKeyboardFunc(&EventLoop::on_keyboard);
-        glutMouseFunc(&EventLoop::on_mouse);
-        glutSpecialFunc(&EventLoop::on_special);
-        glutMainLoop();
-
-    }
+    static void start(int argc, char **argv);
 
 private:
     static void on_display();
